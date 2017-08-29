5c0b093c273cd3326bf6bbae9ee33622ac3fa460
dermon12 <idokuks@gmail.com>
2017-08-29 23:59:33 +0300

    Update main.js
    updated cloud code..

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,148 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define("updateUser", function(request, response) 
+{
+	
+    //Example where an objectId is passed to a cloud function.
+    var id = request.params.user;
+	var requestList = request.params.requestList;
+    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
+    getUser(id).then
+    (   
+        //When the promise is fulfilled function(user) fires, and now we have our USER!
+        function(user)
+        {
+			
+
+			user.set("requestList", requestList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+
+        }
+        ,
+        function(error)
+        {
+            response.error(error);
+        }
+    );
+
+});
+
+function getUser(userId)
+{
+    var userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo("mobile", userId);
+
+    //Here you aren't directly returning a user, but you are returning a function that will sometime in the future return a user. This is considered a promise.
+    return userQuery.first
+    ({
+        success: function(userRetrieved)
+        {
+            //When the success method fires and you return userRetrieved you fulfill the above promise, and the userRetrieved continues up the chain.
+            return userRetrieved;
+        },
+        error: function(error)
+        {
+            return error;
+        }
+    });
+};
+
+
+Parse.Cloud.define("addRec", function(request, response) 
+{
+	
+    //Example where an objectId is passed to a cloud function.
+    var id = request.params.user;
+	var requestList = request.params.sentRecordedMessagesList;
+    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
+    getUser(id).then
+    (   
+        //When the promise is fulfilled function(user) fires, and now we have our USER!
+        function(user)
+        {
+			user.set("sentRecordedMessagesList", requestList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+        }
+        ,
+        function(error)
+        {
+            response.error(error);
+        }
+    );
+
+});
+
+Parse.Cloud.define("updateFriends", function(request, response) 
+{
+	
+    //Example where an objectId is passed to a cloud function.
+    var id = request.params.user;
+	var friendsList = request.params.friendsList;
+	
+    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
+    getUser(id).then
+    (   
+        //When the promise is fulfilled function(user) fires, and now we have our USER!
+        function(user)
+        {
+			if (typeof friendsList === 'string')
+			{
+			var waitingList = user.get("waitingList");
+			waitingList.push(friendsList);
+			user.set("waitingList", waitingList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+			
+			}
+			
+			else
+			{
+			user.set("friendsList", friendsList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+			
+			var driversPoints = user.get("driversPoints");
+			driversPoints.push(0);
+			user.set("driversPoints", pointsList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+			}
+			
+        }
+        ,
+        function(error)
+        {
+            response.error(error);
+        }
+    );
+
+});
+
+
+Parse.Cloud.define("updateWait", function(request, response) 
+{
+	
+    //Example where an objectId is passed to a cloud function.
+    var id = request.params.user;
+	var toput = request.params.toput;
+    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
+    getUser(id).then
+    (   
+        //When the promise is fulfilled function(user) fires, and now we have our USER!
+        function(user)
+        {
+			var waitingList = user.get("waitingList");
+			waitingList.add(toput);
+			user.set("waitingList", waitingList);
+			user.save(null, {useMasterKey:true});
+            response.success("success");
+        }
+        ,
+        function(error)
+        {
+            response.error(error);
+        }
+    );
+
 });
