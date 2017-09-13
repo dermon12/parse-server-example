0580cbc560a4ae192f17c422836e5e33ebf5b186
dermon12 <idokuks@gmail.com>
2017-09-13 22:19:36 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -152,6 +152,7 @@ Parse.Cloud.define("updateWait", function(request, response)
 
 function sendPushNotificationToUserByMobile(mobile, pushData) {
 	var query = new Parse.Query(Parse.Installation);
+	console.log(mobile);
     	query.equalTo("mobile", mobile);
 	Parse.Push.send({
 		  where: query,
