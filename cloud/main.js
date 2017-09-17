d97df84d3b52b4883b3574d6aeb2dfd0572696b8
dermon12 <idokuks@gmail.com>
2017-09-17 20:06:35 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -154,7 +154,7 @@ function sendPushNotificationToUserByMobile(mobile, pushData) {
 	   //Get value from Ticket Object
                   //Set push query
                   var pushQuery = new Parse.Query(Parse.Installation);
-                  pushQuery.containedIn("deviceType",["android"]);
+                  pushQuery.equalesto("deviceType","android");
 
                   //Send Push message
                   Parse.Push.send({
