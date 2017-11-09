830fdc4649e8e8c2e00739b31439438972b93482
dermon12 <idokuks@gmail.com>
2017-11-09 22:05:46 +0200

    Update login.js

--- site/login.js
+++ site/login.js
@@ -1,22 +1,12 @@
 function login() {
     var username = (document.getElementById("username").value);
     var password = (document.getElementById("password").value);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://back-seat.herokuapp.com/parse/functions/SetFactors", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        user: username,
+        pass:password
+    }));
     
-    
-    
-    var url = "https://back-seat.herokuapp.com/parse/functions/SetFactors";
-    var params = "user=" + username + "&pass=" + password;
-    http.open("POST", url, true);
-
-    //Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.setRequestHeader("Content-length", params.length);
-    http.setRequestHeader("Connection", "close");
-
-    http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
-        }
-    }
-    http.send(params);
     }
