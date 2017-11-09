7bac9c19ed7f116ecb974c81cff9ffa88b0e6bee
dermon12 <idokuks@gmail.com>
2017-11-09 23:31:34 +0200

    Update login.js

--- site/login.js
+++ site/login.js
@@ -1,12 +1,12 @@
 function login() {
     var username = (document.getElementById("username").value);
     var password = (document.getElementById("password").value);
-    alert(username);
-    alert(password);
+    
     var Parse = require('parse');
     Parse.initialize("BSId");
     Parse.serverURL = 'http://back-seat.herokuapp.com/'
-    
+    alert(username);
+    alert(Parse);
     Parse.User.logIn(username, password, {
       success: function(user) {
         alert(user);
