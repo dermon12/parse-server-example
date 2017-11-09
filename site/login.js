83c221eee24f62067a76fa7faf1d1ef68759b596
dermon12 <idokuks@gmail.com>
2017-11-09 21:41:57 +0200

    Update login.js

--- site/login.js
+++ site/login.js
@@ -2,5 +2,4 @@ function login() {
     var username = (document.getElementById("username").value);
     var password = (document.getElementById("password").value);
     Parse.Cloud.run('login', { name: username , pass: password});
-			}
 }
