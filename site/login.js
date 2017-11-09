7e0ae06e1786959e78f94a7fdbf85264fb6146aa
dermon12 <idokuks@gmail.com>
2017-11-09 21:19:47 +0200

    Update login.js

--- site/login.js
+++ site/login.js
@@ -2,4 +2,5 @@ function login() {
     alert(document.getElementById("username").value);
     alert(document.getElementById("password").value);
     window.location = "https://stackoverflow.com";
+    return false;
 }
