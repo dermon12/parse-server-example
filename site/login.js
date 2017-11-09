b1acc7cf63bec83875ac7185c27947f198df3720
dermon12 <idokuks@gmail.com>
2017-11-09 22:06:11 +0200

    Update login.js

--- site/login.js
+++ site/login.js
@@ -2,7 +2,7 @@ function login() {
     var username = (document.getElementById("username").value);
     var password = (document.getElementById("password").value);
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://back-seat.herokuapp.com/parse/functions/SetFactors", true);
+    xhr.open("POST", "https://back-seat.herokuapp.com/parse/functions/login", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         user: username,
