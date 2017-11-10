5b50f19e7e178dc30c121e3976c8e1943be6b829
dermon12 <idokuks@gmail.com>
2017-11-10 23:35:14 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -1,5 +1,5 @@
-var session = require('express-session');
 Parse.initialize("BSId");
 Parse.serverURL = 'http://back-seat.herokuapp.com/parse'
 alert("ASDADA" + Parse.User.current());
-alert("hifdsfd");
+var name = "{{ session }}";
+alert(name);
