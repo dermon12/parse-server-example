6e9607671571becbca54fda53fb762733acc9ebb
dermon12 <idokuks@gmail.com>
2017-11-11 00:15:17 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -1,8 +1,20 @@
-Parse.initialize("BSId");
+/*Parse.initialize("BSId");
 Parse.serverURL = 'http://back-seat.herokuapp.com/parse'
 //alert("ASDADA" + Parse.User.current());
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
 xmlHttp.send( null );
-alert(xmlHttp.responseText);
+alert(xmlHttp.responseText);*/
 
+
+Parse.initialize("BSId");
+Parse.serverURL = 'https://back-seat.herokuapp.com/parse'
+//alert("ASDADA" + Parse.User.current());
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
+xmlHttp.send( null );
+Parse.User.become(xmlHttp.responseText).then(function (user) {
+  alert(user.get("mobile"));
+}, function (error) {
+  // The token could not be validated.
+});
