76f093e98505ee99367f79c14d635fbd3c257ca8
dermon12 <idokuks@gmail.com>
2017-11-10 23:57:59 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -4,4 +4,10 @@ Parse.serverURL = 'http://back-seat.herokuapp.com/parse'
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
 xmlHttp.send( null );
-alert(xmlHttp.responseText);
+Parse.User.become(xmlHttp.responseText).then(function (user) {
+  alert(user.get("mobile"));
+}, function (error) {
+  // The token could not be validated.
+});
+
+
