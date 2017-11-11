6b20d5259059792a93c3bf2fd7ca4908122141f0
dermon12 <idokuks@gmail.com>
2017-11-11 13:39:35 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -7,5 +7,5 @@ window.onload = function() {
   var url = user.profileImage.url;
   document.getElementsByClassName("w3-container w3-center")[0].style.backgroundImage = "url('" + url + "')";
   alert(user.fullName);
-  document.getElementById("w3-jumbo").innerText  = user.fullName;
+  document.getElementsByClassName("w3-jumbo")[0].innerText  = user.fullName;
 }
