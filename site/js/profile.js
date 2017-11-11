0e3f5866a0506297d3156012db7f5fdb8554a8d4
dermon12 <idokuks@gmail.com>
2017-11-11 12:18:57 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -5,7 +5,6 @@ var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
 xmlHttp.send( null );
 var user = JSON.parse(xmlHttp.responseText);
-alert(user.mobile);
 document.getElementById("Name").innerHTML = user.fullName;
 document.getElementById("Mail").innerHTML = user.email;
 /*Parse.User.become(xmlHttp.responseText).then(function (user) {
