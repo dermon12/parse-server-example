ceaa79115af626acd4800833301d5caafeb97e22
dermon12 <idokuks@gmail.com>
2017-11-11 11:59:40 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -1,20 +1,13 @@
-/*Parse.initialize("BSId");
-Parse.serverURL = 'http://back-seat.herokuapp.com/parse'
+//Parse.initialize("BSId");
+//Parse.serverURL = 'https://back-seat.herokuapp.com/parse'
 //alert("ASDADA" + Parse.User.current());
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
 xmlHttp.send( null );
-alert(xmlHttp.responseText);*/
-
-
-Parse.initialize("BSId");
-Parse.serverURL = 'https://back-seat.herokuapp.com/parse'
-//alert("ASDADA" + Parse.User.current());
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false for synchronous request
-xmlHttp.send( null );
-Parse.User.become(xmlHttp.responseText).then(function (user) {
+alert(xmlHttp.responseText);
+/*Parse.User.become(xmlHttp.responseText).then(function (user) {
   alert(user.get("mobile"));
 }, function (error) {
   // The token could not be validated.
 });
+*/
