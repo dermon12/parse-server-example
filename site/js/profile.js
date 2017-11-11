dbae94e4ba45f2277ba22c671c42ffda33e585d3
dermon12 <idokuks@gmail.com>
2017-11-11 12:56:22 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -6,9 +6,9 @@ xmlHttp.open( "GET", "https://back-seat.herokuapp.com/token", false ); // false 
 xmlHttp.send( null );
 var user = JSON.parse(xmlHttp.responseText);
 window.onload = function() {
-  alert(user.profileImage);
-  var url = "https://back-seat.herokuapp.com/parse/files/BSId/" + user.profileImage;
-  alert(String(url));
+  alert(user.profileImage.toString());
+  var url = "https://back-seat.herokuapp.com/parse/files/BSId/" + user.profileImage.toString();
+  alert(url);
   document.body.style.backgroundImage = "url('" + url + "')";
 }
 /*Parse.User.become(xmlHttp.responseText).then(function (user) {
