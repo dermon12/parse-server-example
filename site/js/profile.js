dfa23cc4b6aa924ca0f1f701e9e65eea755a64f6
dermon12 <idokuks@gmail.com>
2017-11-11 14:08:50 +0200

    Update profile.js

--- site/js/profile.js
+++ site/js/profile.js
@@ -8,3 +8,13 @@ window.onload = function() {
   document.getElementsByClassName("w3-container w3-center")[0].style.backgroundImage = "url('" + url + "')";
   document.getElementsByClassName("w3-jumbo")[0].innerText  = "Welcome " + user.fullName;
 }
+settable();
+
+function settable(){
+var table = document.getElementById("myTable");
+var row = table.insertRow(-1);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+cell1.innerHTML = "ד1";
+cell2.innerHTML = "10 נקודות"; 
+}
