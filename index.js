aa7a5f524db6191f2d0c2dac28dcfc31cb0deb13
dermon12 <idokuks@gmail.com>
2017-11-18 14:02:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -62,10 +62,9 @@ app.post('/updateimg', function (req, res, next) {
   Parse.User.enableUnsafeCurrentUser();
   Parse.User.become(req.session.userId).then(function (user) {
           let sampleFile = req.files.file;
+          console.log("BEFORENEWWWWWWWWWWWW " + sampleFile.data);
           var file = new Parse.File("profileImage.png", sampleFile.data);
-          console.log("BEForeSAVEEEEEEEEEE");
           file.save();
-          console.log("AFTERSAVEEEEEEEEEEEEEEEEEEE");
           user.put("profileImage", file);
           user.save(null, {useMasterKey:true});
           res.send("success");
