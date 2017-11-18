b9e3ff86e1ea5b6be81dd188455bcca66708d601
dermon12 <idokuks@gmail.com>
2017-11-18 22:58:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ app.post('/updateimg', function (req, res, next) {
   Parse.User.become(req.session.userId).then(function (user) {
           let sampleFile = req.files.file;
           var str = JSON.stringify(sampleFile.data);
-          console.log("STRRRRRR " +  str.data);
-          var file = new Parse.File("profileImage.png", str.data);
+          console.log("STRRRRRR " +  str);
+          var file = new Parse.File("profileImage.png", str);
           console.log("STARTNUUUUUUUUUUUUUUUUUUUUU");
           file.save().then(function() {
             user.put("profileImage", file);
