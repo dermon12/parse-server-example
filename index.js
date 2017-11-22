b309fcd0c10ac1a4b142939859e3a8a12766e2bf
dermon12 <idokuks@gmail.com>
2017-11-22 20:31:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,25 @@ app.get('/', function(req, res) {
    
 
 });
+app.get('/getuser', function (req, res, next) {
+  Parse.User.enableUnsafeCurrentUser();
+  Parse.User.become(req.session.userId).then(function (user) {
+      var numtoget =  req.param('num');
+      var userQuery = new Parse.Query(Parse.User);
+      userQuery.equalTo("mobile", req.body.smobile);
+      userQuery.first({
+        success: function(object) {
+          return res.send(object);
+      },
+      error: function(error) {
+             return res.redirect('/');
+      }
+    }, function (error) {
+      return res.redirect('/');
+    });
+});
+
+
 
 
 app.get('/addfriend', function (req, res, next) {
