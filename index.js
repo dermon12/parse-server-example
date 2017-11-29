35a8f020d8e7f41f2f9ecaa2200431c5a652577c
dermon12 <idokuks@gmail.com>
2017-11-29 22:06:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post('/sendreq', function (req, res, next) {
 				sentrecord[userphone] = file;
 				  console.log("AFTERRRRR " + JSON.stringify(sentrecord));
 				  Parse.Cloud.run('addRec', { user: toset, sentRecordedMessagesList:sentrecord }).then(function(response) {
-					return res.send("OK");	
+					//return res.send("OK");	
 					  return res.redirect('/profile');
 					});
 			  }, function(error) {
