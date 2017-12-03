4853d72442da28eed386ebfbd10a3d61c10efd27
dermon12 <idokuks@gmail.com>
2017-12-03 20:20:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -288,7 +288,16 @@ function verifycode(number, code){
 		return false;      
 	      }
 	      else{
-		  return true;      
+		  object.destroy({
+			  success: function(myObject) {
+			    // The object was deleted from the Parse Cloud.
+			  },
+			  error: function(myObject, error) {
+			    // The delete failed.
+			    // error is a Parse.Error with an error code and message.
+			  }
+			});
+		  return true;
 	      }
 	      
       },
